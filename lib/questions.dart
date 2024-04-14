74e8869033987c406a75e6b00842ae6b8028f89d
PrinceRathee01 <pranaya107@gmail.com>
2024-04-14 20:15:50 +0530

    Project Completed

--- lib/questions.dart
+++ lib/questions.dart
@@ -2,9 +2,6 @@ class Question {
   late String questionText;
   late bool questionAnswer;
 
-  Question( String q,  bool a) {// Using required keyword to indicate that q and a are mandatory parameters
-        questionText = q; // Initializing questionText with the value of q
-        questionAnswer = a; // Initializing questionAnswer with the value of a
-}
+  Question(this.questionText,this.questionAnswer);
   
 }
